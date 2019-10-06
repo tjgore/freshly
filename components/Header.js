8e798ad7d356e9edeef7842fca36d44909bae905
TJ Gore <hello@tjwgore.com>
2019-10-06 00:38:38 -0400

    Added navigation and about page

--- components/Header.js
+++
@@ -1,7 +0,0 @@
-const Header = () => (
-    <div>
-        <h1>Header</h1>
-    </div>
-)
-
-export default Header
--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,9 @@
-import Header from './Header'
 import Footer from './Footer'
+import Nav from './Nav'
 
 const Layout = (props) => (
     <div>
-      <Header/>
+      <Nav/>
         {props.children}
       <Footer/>
     </div>
